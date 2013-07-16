46153eb296204aac99fe69eb4e8c4356d6bfa452
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-07-17 00:23:23 +0400

    add libs

---
+++ libs/backbone.raphael.js
@@ -0,0 +1,45 @@
+Backbone.RaphaelView = Backbone.View.extend({
+
+    delegated: false,
+
+    setElement: function(element, delegate, undelegateEvents) {
+        if (this.el && undelegateEvents) this.undelegateEvents();
+        // el and $el will be the same, $el would have no special meaning...
+        this.el = this.$el = element;
+        if (delegate !== false) this.delegateEvents();
+        return this;
+    },
+
+    delegateEvents: function(events, undelegateEvents) {
+        if (!(events || (events = _.result(this, 'events')))) return this;
+        if (this.delegated) return this;
+        this.delegated = true;
+
+        if(undelegateEvents) this.undelegateEvents();
+        for (var eventName in events) {
+            var method = events[eventName];
+            if (!_.isFunction(method)) method = this[events[eventName]];
+            if (!method) continue;
+
+            method = _.bind(method, this);
+            //If it is one of the svg/vml events
+            if(this.el[eventName]){
+                this.el[eventName](method);
+            }
+            // Custom events for RaphaelView object
+            else{
+                this.on(eventName, method);
+            }
+
+        }
+        return this;
+    },
+
+    // Clears all callbacks previously bound to the view with `delegateEvents`.
+    undelegateEvents: function() {
+        if(this.el.type) this.el.unbindAll();
+        return this;
+    }
+
+});
+
