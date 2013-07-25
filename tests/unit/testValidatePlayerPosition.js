bbc085e2ce57a87c9fad4b8532be8b86a43ba36c
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-07-25 22:31:21 +0400

    tests for validate player position (2, 3 players on the line)

--- tests/unit/testValidatePlayerPosition.js
+++ tests/unit/testValidatePlayerPosition.js
@@ -1,16 +1,14 @@
 TestCase("Test Validate Player Position", {
     setUp: function() {
-        this.board = new BoardModel();
+        this.board = new BoardModel({
+            playersCount: 4
+        });
     },
     "test Valid Position With One Player At The Top": new TestWithProvider({
         data: [
-            {
-                input: {x: 4, y: 1}
-            }, {
-                input: {x: 5, y: 0}
-            }, {
-                input: {x: 3, y: 0}
-            }
+            {input: {x: 4, y: 1}},
+            {input: {x: 5, y: 0}},
+            {input: {x: 3, y: 0}}
         ],
         test: function(input, expected) {
             var player = new PlayerModel({x: 4, y: 0});
@@ -20,15 +18,10 @@ TestCase("Test Validate Player Position", {
     }),
     "test InValid Position With One Player At The Top": new TestWithProvider({
         data: [
-            {
-                input: {x: 5, y: 1}
-            }, {
-                input: {x: 3, y: 1}
-            }, {
-                input: {x: 4, y: -1}
-            }, {
-                input: {x: 4, y: 0}
-            }
+            {input: {x: 5, y: 1}},
+            {input: {x: 3, y: 1}},
+            {input: {x: 4, y: -1}},
+            {input: {x: 4, y: 0}}
         ],
         test: function(input, expected) {
             var player = new PlayerModel({x: 4, y: 0});
@@ -38,13 +31,9 @@ TestCase("Test Validate Player Position", {
     }),
     "test Valid Position With One Player At The Bottom": new TestWithProvider({
         data: [
-            {
-                input: {x: 4, y: 7}
-            }, {
-                input: {x: 5, y: 8}
-            }, {
-                input: {x: 3, y: 8}
-            }
+            {input: {x: 4, y: 7}},
+            {input: {x: 5, y: 8}},
+            {input: {x: 3, y: 8}}
         ],
         test: function(input, expected) {
             var player = new PlayerModel({x: 4, y: 8});
@@ -54,20 +43,127 @@ TestCase("Test Validate Player Position", {
     }),
     "test InValid Position With One Player At The Bottom": new TestWithProvider({
         data: [
-            {
-                input: {x: 3, y: 7}
-            }, {
-                input: {x: 5, y: 7}
-            }, {
-                input: {x: 4, y: 8}
-            }, {
-                input: {x: 4, y: 9}
-            }
+            {input: {x: 3, y: 7}},
+            {input: {x: 5, y: 7}},
+            {input: {x: 4, y: 8}},
+            {input: {x: 4, y: 9}}
         ],
         test: function(input, expected) {
             var player = new PlayerModel({x: 4, y: 8});
             var res = this.board.isValidPlayerPosition(player, input.x, input.y);
             assertFalse(res);
+        }
+    }),
+    "test Valid Position With Two Players (current player - Left)": new TestWithProvider({
+        data: [
+            {input: {x: 4, y: 3},expected: true},
+            {input: {x: 4, y: 5},expected: true},
+            {input: {x: 6, y: 4},expected: true},
+            {input: {x: 3, y: 4},expected: true},
+            {input: {x: 5, y: 3},expected: false},
+            {input: {x: 5, y: 4},expected: false},
+            {input: {x: 5, y: 5},expected: false}
+        ],
+        test: function(input, expected) {
+            var player1 = this.board.players.at(0).set({x: 4, y: 4});
+            var player2 = this.board.players.at(1).set({x: 5, y: 4});
+            var res = this.board.isValidPlayerPosition(player1, input.x, input.y);
+            assertEquals(res, expected);
+        }
+    }),
+    "test Valid Position With Two Players (current player - Right)": new TestWithProvider({
+        data: [
+            {input: {x: 4, y: 3},expected: false},
+            {input: {x: 4, y: 5},expected: false},
+            {input: {x: 6, y: 4},expected: true},
+            {input: {x: 3, y: 4},expected: true},
+            {input: {x: 5, y: 3},expected: true},
+            {input: {x: 5, y: 4},expected: false},
+            {input: {x: 5, y: 5},expected: true}
+        ],
+        test: function(input, expected) {
+            var player1 = this.board.players.at(0).set({x: 4, y: 4});
+            var player2 = this.board.players.at(1).set({x: 5, y: 4});
+            var res = this.board.isValidPlayerPosition(player2, input.x, input.y);
+            assertEquals(res, expected);
+        }
+    }),
+    "test Valid With Three Players (current player - Left)": new TestWithProvider({
+        data: [
+            {input: {x: 3, y: 3}, expected: false},
+            {input: {x: 3, y: 4}, expected: true},
+            {input: {x: 3, y: 5}, expected: false},
+            {input: {x: 4, y: 3}, expected: true},
+            {input: {x: 4, y: 4}, expected: false},
+            {input: {x: 4, y: 5}, expected: true},
+            {input: {x: 5, y: 3}, expected: true},
+            {input: {x: 5, y: 4}, expected: false},
+            {input: {x: 5, y: 5}, expected: true},
+            {input: {x: 6, y: 3}, expected: false},
+            {input: {x: 6, y: 4}, expected: false},
+            {input: {x: 6, y: 5}, expected: false},
+            {input: {x: 7, y: 3}, expected: false},
+            {input: {x: 7, y: 4}, expected: false},
+            {input: {x: 7, y: 5}, expected: false},
+        ],
+        test: function(input, expected) {
+            var player1 = this.board.players.at(0).set({x: 4, y: 4});
+            var player2 = this.board.players.at(1).set({x: 5, y: 4});
+            var player3 = this.board.players.at(2).set({x: 6, y: 4});
+            var res = this.board.isValidPlayerPosition(player1, input.x, input.y);
+            assertEquals(res, expected);
+        }
+    }),
+    "test Valid With Three Players (current player - Middle)": new TestWithProvider({
+        data: [
+            {input: {x: 3, y: 3}, expected: false},
+            {input: {x: 3, y: 4}, expected: true},
+            {input: {x: 3, y: 5}, expected: false},
+            {input: {x: 4, y: 3}, expected: false},
+            {input: {x: 4, y: 4}, expected: false},
+            {input: {x: 4, y: 5}, expected: false},
+            {input: {x: 5, y: 3}, expected: true},
+            {input: {x: 5, y: 4}, expected: false},
+            {input: {x: 5, y: 5}, expected: true},
+            {input: {x: 6, y: 3}, expected: false},
+            {input: {x: 6, y: 4}, expected: false},
+            {input: {x: 6, y: 5}, expected: false},
+            {input: {x: 7, y: 3}, expected: false},
+            {input: {x: 7, y: 4}, expected: true},
+            {input: {x: 7, y: 5}, expected: false}
+        ],
+        test: function(input, expected) {
+            var player1 = this.board.players.at(0).set({x: 4, y: 4});
+            var player2 = this.board.players.at(1).set({x: 5, y: 4});
+            var player3 = this.board.players.at(2).set({x: 6, y: 4});
+            var res = this.board.isValidPlayerPosition(player2, input.x, input.y);
+            assertEquals(res, expected);
+        }
+    }),
+    "test Valid With Three Players (current player - Right)": new TestWithProvider({
+        data: [
+            {input: {x: 3, y: 3}, expected: false},
+            {input: {x: 3, y: 4}, expected: false},
+            {input: {x: 3, y: 5}, expected: false},
+            {input: {x: 4, y: 3}, expected: false},
+            {input: {x: 4, y: 4}, expected: false},
+            {input: {x: 4, y: 5}, expected: false},
+            {input: {x: 5, y: 3}, expected: true},
+            {input: {x: 5, y: 4}, expected: false},
+            {input: {x: 5, y: 5}, expected: true},
+            {input: {x: 6, y: 3}, expected: true},
+            {input: {x: 6, y: 4}, expected: false},
+            {input: {x: 6, y: 5}, expected: true},
+            {input: {x: 7, y: 3}, expected: false},
+            {input: {x: 7, y: 4}, expected: true},
+            {input: {x: 7, y: 5}, expected: false}
+        ],
+        test: function(input, expected) {
+            var player1 = this.board.players.at(0).set({x: 4, y: 4});
+            var player2 = this.board.players.at(1).set({x: 5, y: 4});
+            var player3 = this.board.players.at(2).set({x: 6, y: 4});
+            var res = this.board.isValidPlayerPosition(player3, input.x, input.y);
+            assertEquals(res, expected);
         }
     })
 
