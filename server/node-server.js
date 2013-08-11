8441da2ce81ce8c4224cdb8e66235437689d654d
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-08-11 21:52:15 +0400

    change paths in configs

--- server/node-server.js
+++ server/node-server.js
@@ -1,5 +1,5 @@
 var connect = require('connect')
     , http = require('http')
-    , app = connect().use(connect.static(__dirname + '/'));
+    , app = connect().use(connect.static(__dirname + '/../build'));
 
 http.createServer(app).listen(process.env.PORT || 3000);
