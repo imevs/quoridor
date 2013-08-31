15061310d0c0f79e3a2cd49b69af538e865a7137
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-08-31 14:06:24 +0400

    tests for multiplayer

--- server/game_.js
+++ server/game_.js
@@ -11,19 +11,43 @@ var Game = Backbone.Collection.extend({
     start: function(io) {
         var self = this;
         var room = new Room();
-        this.add(room);
+        self.add(room);
 
-        this.io = io;
-        io.sockets.on('connection', _(self.addPlayer).bind(this));
+        self.io = io;
+        io.sockets.on('connection', _(self.addPlayer).bind(self));
+        io.sockets.on('disconnect', _(self.disconnectPlayer).bind(self));
+    },
+    disconnectPlayer: function(socket) {
+        var playerId = socket && socket.id && socket.id.toString();
+        var room = this.findPlayerRoom(socket);
+        var player = room.players.findWhere({id: playerId});
+        room.players.remove(player);
+    },
+    findPlayerRoom: function(socket) {
+        var playerId = socket && socket.id && socket.id.toString();
+        var room = this.find(function(room) {
+            return room.players.findWhere({id: playerId});
+        });
+        return room;
+    },
+    findFreeRoom: function() {
+        var room = this.find(function(room) {
+            return room.players.length < 2;
+        });
+        return room;
     },
     addPlayer: function(socket) {
-        var room = this.at(0);
         var playerId = socket && socket.id && socket.id.toString();
-        var result = room.addPlayer(playerId);
+        var room = this.findFreeRoom();
+        var result = false;
+        if (!!room) {
+            result = room.addPlayer(playerId);
+        }
+
         if (!result) {
             room = new Room();
             this.add(room);
-            result = room.addPlayer(playerId);
+            room.addPlayer(playerId);
         }
     }
 });
--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 var assert = require("assert");
 var Backbone = require('backbone');
-var Game = require('../server/_game.js');
-var Room = require('../server/room.js');
-
+var Game = require('../server/game_.js');
 
 var emitter = require('events').EventEmitter;
 var io = {
@@ -14,11 +12,14 @@ var io = {
 
 describe('Game', function() {
     describe('create Game', function(){
+
         it('First Game create', function(){
             assert.notEqual(false, Game);
             assert.ok(Game.prototype instanceof Backbone.Collection);
             assert.ok(new Game() instanceof Backbone.Collection);
+        });
 
+        it('start game', function() {
             var game = new Game();
             assert.equal(0, game.length);
 
@@ -29,20 +30,102 @@ describe('Game', function() {
             assert.ok(room1 instanceof Backbone.Model);
             assert.ok(room1.players instanceof Backbone.Collection);
             assert.equal(0, room1.players.length);
+        });
 
-            // connect user
+        it('connect 1 player', function() {
+            var game = new Game();
+            game.start(io);
+
             io.sockets.emit('connection', {id: '1'});
+
+            var room1 = game.at(0);
             assert.equal(1, room1.players.length);
+        });
 
+        it('connect 2 player', function() {
+            var game = new Game();
+            game.start(io);
+
+            io.sockets.emit('connection', {id: '1'});
             io.sockets.emit('connection', {id: '2'});
-            assert.equal(2, room1.players.length);
 
+            var room1 = game.at(0);
+            assert.equal(2, room1.players.length);
+        });
+
+        it('connect 3 player', function() {
+            var game = new Game();
+            game.start(io);
+
+            io.sockets.emit('connection', {id: '1'});
+            io.sockets.emit('connection', {id: '2'});
             io.sockets.emit('connection', {id: '3'});
+
+            var room1 = game.at(0);
+            var room2 = game.at(1);
+
             assert.equal(2, room1.players.length);
             assert.equal(2, game.length);
+            assert.equal(1, room2.players.length);
+        });
+
+        it('disconnect player from room 1', function() {
+            var game = new Game();
+            game.start(io);
+
+            io.sockets.emit('connection', {id: '1'});
+            io.sockets.emit('disconnect', {id: '1'});
+
+            assert.equal(1, game.length);
+            var room1 = game.at(0);
+            assert.equal(0, room1.players.length);
+        });
+
+        it("find player's room", function() {
+            var game = new Game();
+            game.start(io);
+
+            io.sockets.emit('connection', {id: '1'});
+            io.sockets.emit('connection', {id: '2'});
+            io.sockets.emit('connection', {id: '3'});
+
+            assert.equal(game.at(0), game.findPlayerRoom({id: '1'}));
+            assert.equal(game.at(1), game.findPlayerRoom({id: '3'}));
+        });
+
+        it('disconnect user from room 2', function() {
+            var game = new Game();
+            game.start(io);
+
+            io.sockets.emit('connection', {id: '1'});
+            io.sockets.emit('connection', {id: '2'});
+            io.sockets.emit('connection', {id: '3'});
+
+            io.sockets.emit('disconnect', {id: '3'});
 
             var room2 = game.at(1);
-            assert.equal(1, room2.players.length);
+            assert.equal(0, room2.players.length);
+        });
+
+        it('findFreeRoom get room 1', function() {
+            var game = new Game();
+            game.start(io);
+
+            var room1 = game.at(0);
+            assert.equal(room1, game.findFreeRoom());
+        });
+
+        it('findFreeRoom get room 2', function() {
+            var game = new Game();
+            game.start(io);
+
+            io.sockets.emit('connection', {id: '1'});
+            io.sockets.emit('connection', {id: '2'});
+            io.sockets.emit('connection', {id: '3'});
+            io.sockets.emit('disconnect', {id: '3'});
+
+            var room2 = game.at(1);
+            assert.equal(room2, game.findFreeRoom());
         });
 
     })
