f6e3a7754eb26cbae1dc1d04af69e963b2292de2
imevs <mvs-05@mail.ru>
2013-12-03 16:20:41 +0300

    change bots types

--- server/routes/bots.js
+++ server/routes/bots.js
@@ -2,7 +2,7 @@ module.exports = function (req, res) {
 
     var playersCount = req.params.playersCount;
 
-    var playerTypes = ['bot', 'bot', 'bot', 'bot'];
+    var playerTypes = ['smartbot', 'smartbot', 'smartbot', 'smartbot'];
     var room = global.game.createNewRoom(playersCount, playerTypes);
 
     room.save({}, {
