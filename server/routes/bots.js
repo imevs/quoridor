1e95e83235d214fb9e052b9ae17f8615b2af604a
Vitaly Medvedev <vmedvedev@evolutiongaming.com>
2022-12-01 23:39:42 +0200

    Remove server scripts

--- server/routes/bots.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res) {
-
-    var playersCount = req.params.playersCount;
-
-    var playerTypes = ['smartbot', 'smartbot', 'smartbot', 'smartbot'];
-    var room = global.game.createNewRoom(playersCount, playerTypes);
-    res.redirect('/play/game/' + room.get('id'));
-};
